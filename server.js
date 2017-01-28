9a779ea0ede9bae199ae0e03daa3b1207bc9807f
Clive <lenzjo@gmail.com>
2017-01-28 10:44:31 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs     = require('hbs');
+const fs      = require('fs');
+
+var app = express();
+
+hbs.registerPartials( __dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+
+// app.use((req, res, next) => {
+//   res.render('maintanance.hbs', {
+//       pageTitle: 'Maintanance Page',
+//       msg: 'This site is undergoing some work, please try again later.'
+//     });
+// });
+
+
+app.use(express.static( __dirname + '/public' ));
+
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    msg: 'Welcome to my page!'
+  });
+});
+
+
+// app.get('/', (req, res) => {
+//   res.render('maintanance.hbs', {
+//     pageTitle: 'Maintanance Page',
+//     msg: 'This site is undergoing some work, please try again later.'
+//   });
+// });
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    status: 'error',
+    msg: 'No such page.'
+  })
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is up on Port 3000');
+});
